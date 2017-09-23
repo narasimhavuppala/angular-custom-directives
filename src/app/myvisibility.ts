a5bfb3c71797507af118ff14d508f7165de71564
nraovuppala@gmail.com <nraovuppala@gmail.com>
2017-09-23 17:39:36 +0530

    Input parameter is added

--- src/app/myvisibility.ts
+++ src/app/myvisibility.ts
@@ -1,21 +1,23 @@
-import { Directive, ElementRef, Renderer, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, Renderer } from '@angular/core';
 
 @Directive({
   selector: '[visibility]'
 })
 export class VisibilityDirective {
 
+  @Input()
   isVisible: boolean;
 
   constructor(private e: ElementRef, private r: Renderer) {
     r.setElementStyle(e.nativeElement, 'font-size', '22px');
-    this.isVisible = false;
+    //this.isVisible = false;
   }
 
 
   // To Listen to Events that are triggered by client
   @HostListener('dblclick')
   foo(isVisible) {
+    console.log(this.isVisible);
     if (isVisible) {
       this.e.nativeElement.style.display = 'block';
     }  else {
